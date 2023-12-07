c8a6d0cb24c8a077e66d19a62eb282ef2262b651
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-07 02:08:20 +0200

    place order

--- place_order.php
+++ place_order.php
@@ -24,5 +24,32 @@ if (!$token) {
     exit();
 }
 
+try {
+    $key = "your_secret";
+    $decoded = JWT::decode($token, new Key($key, 'HS256'));
+
+    // Check if the user has permission to place an order (user type id 3 for customers)
+    if ($decoded->usertype_id == 3) {
+        // POST method to send data
+        $user_id = $decoded->user_id;
+
+        $response = array();
+        $response["status"] = "success";
+        $response["message"] = "Order placed successfully!";
+
+        echo json_encode($response);
+    } else {
+        $response['status'] = 'error';
+        $response['message'] = 'Unauthorized. Only customers (usertype_id = 3) can place orders.';
+        echo json_encode($response);
+    }
+} catch (ExpiredException $e) {
+    http_response_code(401);
+    echo json_encode(["error" => "expired"]);
+} catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode(["error" => "Invalid token"]);
+}
+
 $mysqli->close();
 ?>
