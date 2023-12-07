8bd514b62b1955ef9c140e85ce5a62ba5bd7a3e8
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-07 02:01:57 +0200

    add to cart

---
+++ add_to_cart.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include("connection.php");
+require __DIR__ . '/vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\Key;
+
+$headers = getallheaders();
+
+if (!isset($headers['Authorization']) || empty($headers['Authorization'])) {
+    http_response_code(401);
+    echo json_encode(["error" => "unauthorized"]);
+    exit();
+}
+
+$authorizationHeader = $headers['Authorization'];
+$token = null;
+
+$token = trim(str_replace("Bearer", '', $authorizationHeader));
+if (!$token) {
+    http_response_code(401);
+    echo json_encode(["error" => "unauthorized"]);
+    exit();
+}
+
+?>
